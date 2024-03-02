497350e5a8c3ffdd4a2948d3bf3a8d52323558ff
hasanakoluk <hasanakoluk3535@gmail.com>
2024-03-02 10:05:32 +0300

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	deleted:    src/Course.jsx 	modified:   src/Courses.jsx

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,23 +1,70 @@
-import Course from "./Course";
-function Courses({courses,removeCourses}) {
+
+import { useState } from "react";
+import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
+
+
+function Courses({courses,removeCourses}) { 
+    const [index, setIndex] = useState(0)
+    const {title,price,content} =courses[index]
     
+
+    const checkIndex = (index)=>{
+        console.log(index)
+        if(index < 0){
+            return courses.length -1
+        }
+        if(index > courses.length -1){
+            return 0
+        }
+        return index
+    }
+
+    const prevCourse = ()=>{
+        setIndex((index)=>{
+            let newIndex = index -1
+            return checkIndex(newIndex)
+        })
+    }
+
+    const nextCourse = ()=>{
+        setIndex((index)=>{
+            let newIndex = index +1
+            return checkIndex(newIndex)
+        })
+    }
+
+    const getRandomCourse = ()=>{
+        let randomNumber = Math.floor(Math.random()*courses.length)
+        if(randomNumber === index){
+            randomNumber = index +1
+           
+        }
+        setIndex(checkIndex(randomNumber))
+    }
+
     return ( 
     
     <div className="courseMainDiv">
-        <div>
-            <h1 className="courseTitle">Kurslarım</h1>
+        <div className="courseTitleAndButton">
+            <h1 className="courseTitle">Random Programing Language</h1>
+            <button className="cardDeleteBtn" onClick={getRandomCourse}>Rastgele Kurs Ata</button>
         </div>
         <div className="cardDiv">
-            {
-                courses.map((course)=>{
-                  return(
-                    <Course key={course.id}
-                     {...course}
-                      removeOneCourse={removeCourses}
-                      />
-                  )  
-
-                })}
+                <button className="prewNextBtn" onClick={prevCourse}>
+                    <FaChevronLeft/>
+                </button>
+                <div className="card">
+                <div className="cardTitlePrice">
+                  <h2 className="cardTitle">{title}</h2>
+                  <h4 className="cardPrice">{price}TL</h4>
+                </div>
+                <p>{content}</p>
+                
+              </div>
+                <button className="prewNextBtn" onClick={nextCourse}>
+                    <FaChevronRight/>
+                </button>
+            
         </div>
     </div>
     
